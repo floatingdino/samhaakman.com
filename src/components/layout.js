fe5ec0fe11b514e9f2e7a63a45fd14e3a2a10738
Sam <sam@samhaakman.com>
2020-02-01 11:52:05 +1000

    Header title moved to static

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,28 +7,16 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql, Link } from "gatsby"
+import { Link } from "gatsby"
 
 import Header from "./header"
 
 import "./layout.scss"
 
 const Layout = ({ children, className }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <div className={`layout-wrapper ${className || ""}`}>
-      <Header
-        siteTitle={(data.site && data.site.siteMetadata.title) || "Sam Haakman"}
-      />
+      <Header siteTitle="Sam Haakman" />
 
       <main>
         <div className="grid-container">{children}</div>
