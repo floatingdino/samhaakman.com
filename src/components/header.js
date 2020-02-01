bc0f5051a382d1a5147793aa0bec3e43c963faf5
Sam <sam@samhaakman.com>
2020-02-01 11:22:18 +1000

    mobile menu functionality

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,51 @@
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
 
 import "./header.scss"
 
-const Header = ({ siteTitle }) => (
-  <header className="mb-2">
-    <div className="grid-container">
-      <div className="grid-x align-justify align-middle">
-        <div className="cell auto">
-          <div className="header-logo">
-            <Link to="/">{siteTitle}</Link>
+const Header = ({ siteTitle }) => {
+  const [open, setOpen] = useState(false)
+  return (
+    <header className="mb-2">
+      <div className="grid-container">
+        <div className="grid-x align-justify align-middle">
+          <div className="cell auto">
+            <div className="header-logo">
+              <Link to="/">{siteTitle}</Link>
+            </div>
+          </div>
+          <div className="cell shrink">
+            <div className="hide-for-large">
+              <button
+                onClick={() => setOpen(!open)}
+                className="mobile-menu-opener"
+                aria-controls="main-nav"
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : "false"}
+              >
+                <span class="show-for-sr">Open Mobile Menu</span>
+              </button>
+            </div>
+            <nav
+              className={`main-nav ${(!open && "hidden") || ""}`}
+              id="main-nav"
+            >
+              <ul className="header-nav">
+                <li>
+                  <Link to="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
-        <div className="cell shrink">
-          <nav className="show-for-large">
-            <ul className="header-nav">
-              <li>
-                <Link to="/portfolio">Portfolio</Link>
-              </li>
-              <li>About</li>
-            </ul>
-          </nav>
-          <div className="hide-for-large">MENU</div>
-        </div>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ const IndexPage = ({ data }) => {
                         "Read More"}
                     </Link>
                   </div>
-                  <div className="cell shrink meta">
+                  <div className="cell shrink meta mt-1">
                     {RichText.asText(node.studio)}{" "}
                     <span className="separator" />{" "}
                     <time>
