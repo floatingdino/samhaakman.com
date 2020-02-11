37404db3d34e0b8dde5483c438ed6eb07336a316
Sam Haakman <sam@lambagency.com.au>
2020-02-12 08:37:18 +1000

    mobile menu colour fix, portfolio image sizing tweak

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -77,7 +77,7 @@ export const query = graphql`
             preview_image
             preview_imageSharp {
               childImageSharp {
-                fluid(maxWidth: 590, maxHeight: 560) {
+                fixed(maxWidth: 590, maxHeight: 510) {
                   ...GatsbyImageSharpFluid_withWebp
                 }
               }
