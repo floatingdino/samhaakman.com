b07572c9fbc834e1560094dfaad294956705f65b
Sam <sam@samhaakman.com>
2020-02-12 20:50:40 +1000

    specify case studies for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,8 @@ import "./index.scss"
 
 const IndexPage = ({ data }) => {
   const doc = data.prismic.allHomepages.edges.slice(0, 1).pop()
-  const articles = data.prismic.allPortfos.edges
+  const articles = doc.node.featured_case_studies
+  console.log(articles)
   return (
     <Layout>
       <SEO title="Home" />
@@ -24,43 +25,49 @@ const IndexPage = ({ data }) => {
       {doc && RichText.render(doc.node.body)}
       <div className="article-wrapper">
         {articles &&
-          articles.map(({ node }) => (
-            <article className="card mb-2" key={node._meta.uid}>
+          articles.map(({ case_study }) => (
+            <article className="card mb-2" key={case_study._meta.uid}>
               <div className="grid-x align-justify">
                 <div className="cell large-4 small-8 grid-y">
                   <div className="cell auto grid-y align-center">
                     <h2 className="h1 mb-1">
-                      {RichText.asText(node.teaser || node.title)}
+                      {RichText.asText(case_study.teaser || case_study.title)}
                     </h2>
-                    <Link to={linkResolver(node._meta)} className="arrow-link">
-                      {(node.teaser && RichText.asText(node.title)) ||
+                    <Link
+                      to={linkResolver(case_study._meta)}
+                      className="arrow-link"
+                    >
+                      {(case_study.teaser &&
+                        RichText.asText(case_study.title)) ||
                         "Read More"}
                     </Link>
                   </div>
                   <div className="cell shrink meta mt-1">
-                    {RichText.asText(node.studio)}{" "}
+                    {RichText.asText(case_study.studio)}{" "}
                     <span className="separator" />{" "}
                     <time>
                       {Intl.DateTimeFormat("en-GB", {
                         month: "long",
                         year: "numeric",
-                      }).format(Date(node.project_date))}
+                      }).format(Date(case_study.project_date))}
                     </time>
                   </div>
                 </div>
                 <div className="cell large-6">
-                  {(node.preview_imageSharp &&
-                    node.preview_imageSharp.childImageSharp && (
+                  {(case_study.preview_imageSharp &&
+                    case_study.preview_imageSharp.childImageSharp && (
                       <Img
-                        fluid={node.preview_imageSharp.childImageSharp.fluid}
-                        alt={node.preview_image.alt}
+                        fluid={
+                          case_study.preview_imageSharp.childImageSharp.fluid
+                        }
+                        alt={case_study.preview_image.alt}
                         className="large-card-image"
                         style={{ display: "block" }}
                       />
                     )) || (
                     <img
-                      src={node.preview_image.url}
-                      alt={node.preview_image.alt}
+                      src={case_study.preview_image.url}
+                      alt={case_study.preview_image.alt}
                       className="large-card-image"
                       style={{ display: "block" }}
                     />
@@ -84,27 +91,28 @@ export const query = graphql`
           node {
             title
             body
-          }
-        }
-      }
-      allPortfos(sortBy: project_date_DESC, first: 3) {
-        edges {
-          node {
-            title
-            teaser
-            studio
-            project_date
-            preview_image
-            preview_imageSharp {
-              childImageSharp {
-                fluid(maxHeight: 560) {
-                  ...GatsbyImageSharpFluid_withWebp
+            featured_case_studies {
+              case_study {
+                _linkType
+                ... on PRISMIC_Portfo {
+                  title
+                  teaser
+                  studio
+                  project_date
+                  preview_image
+                  preview_imageSharp {
+                    childImageSharp {
+                      fluid(maxHeight: 560) {
+                        ...GatsbyImageSharpFluid_withWebp
+                      }
+                    }
+                  }
+                  _meta {
+                    uid
+                    type
+                  }
                 }
               }
-            }
-            _meta {
-              uid
-              type
             }
           }
         }
