4189279b25ddb6ed22122184f15461cbc9a4d7f9
Sam <sam@samhaakman.com>
2020-02-14 21:44:31 +1000

    Updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,12 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>That's a 404, my friend</h1>
+    <p>Nothing doing here, move along.</p>
+    <img
+      src="https://thumbs.gfycat.com/BronzeDizzyGrasshopper-size_restricted.gif"
+      alt="not the droids you're looking for"
+    />
   </Layout>
 )
 
