201bfa6cc24a04680e6c3ec1288bd4c2c504963b
Sam <samhaakman@josephmark.com.au>
2021-02-09 22:23:35 +1000

    plz

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,7 @@ module.exports = {
     collect: {
       staticDistDir: "./public",
       autodiscoverUrlBlocklist:
-        "http://localhost:34761/offline-plugin-app-shell-fallback/index.html",
+        "http://localhost/offline-plugin-app-shell-fallback/index.html",
     },
     upload: {
       target: "temporary-public-storage",
