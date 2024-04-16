f82de3aafbe37e344994eccc214a7969f9578b72
Sam Haakman <sam@josephmark.studio>
2024-04-16 22:32:35 +1000

    underline links in richtext

--- src/components/Prismic.tsx
+++ src/components/Prismic.tsx
@@ -11,6 +11,11 @@ const COMPONENTS = {
   heading2: ({ children }) => <H2 className="mb-10">{children}</H2>,
   heading3: ({ children }) => <H3 className="mb-[1em]">{children}</H3>,
   paragraph: ({ children }) => <p className="mb-9">{children}</p>,
+  hyperlink: ({ children, node: { data } }) => (
+    <a href={data.url} className="underline" target={data.target}>
+      {children}
+    </a>
+  ),
 }
 
 const PrismicRichText: typeof OPrismicRichText = (props) => {
