df58bec39ee1250a3eab96dba05b763567a16f17
Sam Haakman <sam@josephmark.studio>
2024-04-16 17:43:47 +1000

    add general page

---
+++ app/(main)/[uid]/page.tsx
@@ -0,0 +1,45 @@
+import { Container } from "@/components/Container"
+import { PrismicRichText, PrismicText } from "@/components/Prismic"
+import { H1 } from "@/components/Type"
+import API from "@/utils/api"
+import { Metadata } from "next"
+import { notFound } from "next/navigation"
+
+const getPage = async (uid: string) => {
+  const { data } = await API.get({ q: `[[at(my.page.uid, "${uid}")]]` })
+
+  return data.results.pop()
+}
+
+export const revalidate = 60
+
+export async function generateMetadata({ params }): Promise<Metadata> {
+  const page = await getPage(params.uid)
+  return {
+    title: page.data.page_title.pop().text,
+  }
+}
+export default async function Page({ params }) {
+  const page = await getPage(params.uid)
+
+  if (!page) {
+    notFound()
+  }
+  return (
+    <Container className="mb-20">
+      <style>
+        {`:root {
+          --bg-color-main: #fff;
+          --fg-color: #000;
+          --bg-image-main: none;
+        }`}
+      </style>
+      <div className="lg:w-1/2 mx-auto">
+        <H1 className="mb-10">
+          <PrismicText field={page.data.page_title} />
+        </H1>
+        <PrismicRichText field={page.data.body} />
+      </div>
+    </Container>
+  )
+}
