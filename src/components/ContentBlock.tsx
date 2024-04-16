9aa58daebebc5665c1e0a67e75accbc531a339d6
Sam Haakman <sam@josephmark.studio>
2024-04-16 22:18:17 +1000

    fix content block if no image

--- src/components/ContentBlock.tsx
+++ src/components/ContentBlock.tsx
@@ -5,23 +5,25 @@ import { PrismicRichText } from "./Prismic"
 
 const ContentBlock = ({ body, originalImage, alt, ...props }) => (
   <div className="portfolio-slice my-20">
-    <div className="flex flex-wrap gap-7 items-center">
+    <div className="flex flex-wrap gap-7 items-center justify-between">
       <div
-        className="lg:w-1/2"
+        className="lg:w-5/12"
         style={{ order: (props.alternateLayout && "1") || null }}
       >
         <PrismicRichText field={body} />
       </div>
-      <div className="flex-1 shrink-0">
-        <Image
-          src={originalImage.url}
-          alt={alt || originalImage.alt}
-          className="mt-2 mb-2"
-          width="606"
-          height="606"
-          style={{ height: "auto" }}
-          unoptimized
-        />
+      <div className="shrink-0 lg:w-1/2">
+        {originalImage?.url && (
+          <Image
+            src={originalImage.url}
+            alt={alt || originalImage.alt}
+            className="mt-2 mb-2"
+            width="606"
+            height="606"
+            style={{ height: "auto" }}
+            unoptimized
+          />
+        )}
       </div>
     </div>
   </div>
