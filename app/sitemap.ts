3d376228ca54ef0858fa6ccf736c57f10e62e499
Sam Haakman <sam@josephmark.studio>
2024-04-26 09:20:46 +1000

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+import API from "@/utils/api"
+import { MetadataRoute } from "next"
+
+const STATIC_ROUTES = ["", "/portfolio"]
+const CANONICAL_DOMAIN = "https://samhaakman.com"
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const [{ data: pages }, { data: portfolio }] = await Promise.all([
+    API.get({ q: '[[at(document.type, "page")]]' }),
+    API.get({ q: '[[at(document.type, "portfo")]]' }),
+  ])
+
+  const pageRoutes = pages.results.map((page) => ({
+    url: new URL(`/${page.uid}`, CANONICAL_DOMAIN).toString(),
+    lastmod: page.last_publication_date,
+  }))
+
+  const portfolioRoutes = portfolio.results.map((caseStudy) => ({
+    url: new URL(`/portfolio/${caseStudy.uid}`, CANONICAL_DOMAIN).toString(),
+    lastmod: caseStudy.last_publication_date,
+  }))
+
+  const staticRoutes = STATIC_ROUTES.map((route) => ({
+    url: new URL(route, CANONICAL_DOMAIN).toString(),
+  }))
+
+  return [...staticRoutes, ...pageRoutes, ...portfolioRoutes]
+}
